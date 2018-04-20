0cdf30f72bd4b816161b72dddd83a161a0851afa
Boy Wang <boyw165@gmail.com>
2018-04-21 02:50:14 +0800

    Add IPenColorRepo and corresponding file implementation. #27

---
+++ app-model/src/main/java/com/paper/model/Color.kt
@@ -0,0 +1,40 @@
+// Copyright Apr 2018-present boyw165@gmail.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+package com.paper.model
+
+object Color {
+
+    fun parseColor(colorString: String): Int {
+        if (colorString[0] == '#') {
+            // Use a long to avoid rollovers on #ffXXXXXX
+            var color = java.lang.Long.parseLong(colorString.substring(1), 16)
+            if (colorString.length == 7) {
+                // Set the alpha value
+                color = color or -0x1000000
+            } else if (colorString.length != 9) {
+                throw IllegalArgumentException("Unknown color")
+            }
+            return color.toInt()
+        } else {
+            throw IllegalArgumentException("Unknown color")
+        }
+    }
+}
